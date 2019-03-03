de1918467244307458f71955d98f35c91fb73470
Nathan Thomas <nwthomas@me.com>
2019-03-02 18:17:25 -0800

    Add charity update route

--- charities/charitiesRouter.js
+++ charities/charitiesRouter.js
@@ -4,6 +4,7 @@ const tokenService = require("../auth/tokenService.js");
 
 const router = express.Router();
 
+// Get all charities in database route
 router.get("/", async (req, res) => {
   try {
     const charities = await Charities.find();
@@ -19,12 +20,12 @@ router.get("/", async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({
-      message: "There was an error retrieving the charities from the database.",
-      error
+      message: "There was an error retrieving the charities from the database."
     });
   }
 });
 
+// Get charity by ID route
 router.get("/:id", async (req, res) => {
   try {
     const charities = await Charities.findById(req.params.id);
@@ -36,12 +37,37 @@ router.get("/:id", async (req, res) => {
     } else {
       res
         .status(404)
-        .json({ message: "The charity could not be found in the databse." });
+        .json({ message: "The charity could not be found in the database." });
     }
   } catch (error) {
     res.status(500).json({
-      message: "There was an error retrieving the charity from the database.",
-      error
+      message: "There was an error retrieving the charity from the database."
+    });
+  }
+});
+
+// Update charity route
+router.put("/:id", async (req, res) => {
+  if (!req.body.name) {
+    res
+      .status(404)
+      .json({ message: "Please include a name to update and try again." });
+  }
+  try {
+    const updatedCharity = await Charities.update(req.params.id, req.body);
+    if (updatedCharity) {
+      res.status(200).json({
+        message: "The charity was updated in the database.",
+        numUpdated: updatedCharity
+      });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The charity could not be updated in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error updating the charity in the database."
     });
   }
 });
