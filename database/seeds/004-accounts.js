c5328b23c77ce5cf0393fd22db6e602378cabe01
Nathan Thomas <nwthomas@me.com>
2019-03-21 22:09:45 -0700

    Update accounts table seed data

--- database/seeds/004-accounts.js
+++ database/seeds/004-accounts.js
@@ -4,28 +4,24 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return knex("accounts").insert([
         {
-          first_name: "Nathan",
-          last_name: "Thomas",
           username: "nwthomas",
-          password: bcrypt.hashSync("asdfasdf", 14)
+          password: bcrypt.hashSync("asdfasdf", 14),
+          email: "email@me.com"
         },
         {
-          first_name: "Admin",
-          last_name: "Admin",
           username: "admin",
-          password: bcrypt.hashSync("password", 14)
+          password: bcrypt.hashSync("password", 14),
+          email: "admin@gmail.com"
         },
         {
-          first_name: "Veronica",
-          last_name: "Smith",
           username: "vero2019",
-          password: bcrypt.hashSync("password", 14)
+          password: bcrypt.hashSync("password", 14),
+          email: "vero2019@gmail.com"
         },
         {
-          first_name: "Steven",
-          last_name: "Taylor",
           username: "staytay",
-          password: bcrypt.hashSync("password", 14)
+          password: bcrypt.hashSync("password", 14),
+          email: "candycane301@gmail.com"
         }
       ]);
     });
