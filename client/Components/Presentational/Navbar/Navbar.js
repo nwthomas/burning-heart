fcd25c69d872b0b738d9aef6491bcea0b8f3b604
Nathan Thomas <nwthomas@me.com>
2019-03-21 21:32:15 -0700

    Update tables for migration data SQL

--- client/Components/Presentational/Navbar/Navbar.js
+++ client/Components/Presentational/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { Link, TouchableOpacity } from "react-router-native";
 
