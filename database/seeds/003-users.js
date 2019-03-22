88a2acf41b61a1ef7fad87c23318747089deb90b
Nathan Thomas <nwthomas@me.com>
2019-03-21 22:21:23 -0700

    Implement charityContact table seed data

--- database/seeds/003-users.js
+++ database/seeds/003-users.js
@@ -16,20 +16,6 @@ exports.seed = function(knex, Promise) {
           middleName: "Admin",
           lastName: "Admin",
           phone: 9098832912
-        },
-        {
-          accountId: 3,
-          firstName: "Veronica",
-          middleName: "Mary",
-          lastName: "Smith",
-          phone: 3037892873
-        },
-        {
-          accountId: 4,
-          firstName: "Steven",
-          middleName: "James",
-          lastName: "Taylor",
-          phone: 5108943782
         }
       ]);
     });
--- database/seeds/005-charityContacts.js
+++ database/seeds/005-charityContacts.js
@@ -1,11 +1,28 @@
+const faker = require("faker");
+
 exports.seed = function(knex, Promise) {
-  return knex("table_name")
+  return knex("charityContacts")
     .del()
     .then(function() {
-      return knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+      return knex("charityContacts").insert([
+        {
+          charityId: 2,
+          accountId: 3,
+          firstName: "Veronica",
+          middleName: "Mary",
+          lastName: "Smith",
+          phone: 3037892873,
+          driversLicense: "D73DH8KA90"
+        },
+        {
+          charityId: 1,
+          accountId: 4,
+          firstName: "Steven",
+          middleName: "James",
+          lastName: "Taylor",
+          phone: 5108943782,
+          driversLicense: "D73DH8KA99"
+        }
       ]);
     });
 };
