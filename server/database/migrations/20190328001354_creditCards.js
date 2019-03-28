9e7fb2ec5f746e612f1790a575d2fa7b72245082
Nathan Thomas <nwthomas@me.com>
2019-03-28 00:20:59 -0700

    Create creditCards table data structures

---
+++ server/database/migrations/20190328001354_creditCards.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.makeTable("creditCards", tbl => {
+    tbl.increments("id");
+    tbl
+      .integer("cardNumber")
+      .unsigned()
+      .notNullable();
+    tbl.string("expDate", 128).notNullable();
+    tbl
+      .integer("securityCode")
+      .unsigned()
+      .notNullable();
+    tbl
+      .integer("accountId")
+      .unsigned()
+      .references("id")
+      .inTable("account")
+      .onDelete("CASCADE")
+      .notNullable();
+    tbl.string("cardName", 256).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("creditCards");
+};
