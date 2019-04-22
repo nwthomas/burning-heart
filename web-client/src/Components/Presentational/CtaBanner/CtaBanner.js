bf5ad4885b08417904a2e5d6bb998cf5a013d4e2
Nathan Thomas <nwthomas@me.com>
2019-04-21 19:18:20 -0700

    Finish all mobile layout other than navbar menu

--- web-client/src/Components/Presentational/CtaBanner/CtaBanner.js
+++ web-client/src/Components/Presentational/CtaBanner/CtaBanner.js
@@ -5,7 +5,7 @@ const CtaBanner = () => {
     <div className="cta__banner">
       <p className="cta__banner__text">
         Burning Heart is a donation app for people who like to give what they
-        can when they can.{" "}
+        can, when they can.{" "}
         <a className="cta__banner__link" href="#">
           Sign up here.
         </a>
